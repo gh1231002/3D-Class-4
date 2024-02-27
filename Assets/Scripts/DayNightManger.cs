28ed0b3e051884d90d70be5e98353d2725d4afd7
gh1231002 <gh1231002@naver.com>
2024-02-27 19:53:52 +0900

    24.02.27

--- Assets/Scripts/DayNightManger.cs
+++ Assets/Scripts/DayNightManger.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DayNightManger : MonoBehaviour
@@ -10,6 +11,9 @@ public class DayNightManger : MonoBehaviour
     public bool isNight = false;
     private string keyIsNight = " isNight";
     public bool AutoChange = false;//자동 낮밤 변경
+
+    [SerializeField,Range(0f,24f)] float dayTime = 14;
+    [SerializeField,Range(0f,24f)] float nightTime = 23;
 
     List<FunctionLight> listhLights = new List<FunctionLight>();
 
@@ -36,8 +40,14 @@ public class DayNightManger : MonoBehaviour
                 }
                 else
                 {
-                    listhLights.Add(light.transform.GetComponent<FunctionLight>());
+                    listhLights.Add(light.transform.parent.GetComponent<FunctionLight>());
                 }
+            }
+            count = listhLights.Count;
+            for(int iNum = 0; iNum < count; ++iNum)
+            {
+                FunctionLight light = listhLights[iNum];
+                light.init(isNight);
             }
         }
     }
@@ -52,17 +62,82 @@ public class DayNightManger : MonoBehaviour
         timeOfDay %= 24;//0 ~ 24 사이의 값만 저장
         if(AutoChange == true)
         {
-            timeOfDay += Time.deltaTime;//시간이 자동으로 증가
+            //timeOfDay += Time.deltaTime;//시간이 자동으로 증가
+            if (timeOfDay > dayTime)//14
+            {
+                isNight = true;
+            }
+            else if(timeOfDay > nightTime)//23
+            {
+                isNight = false;
+            }
         }
+
         else//시간을 직접 관리
         {
             if(isNight == true)
             {
-                timeOfDay = 19;//밤
+                timeOfDay += Time.deltaTime;
+                if(timeOfDay > nightTime)
+                {
+                    timeOfDay = nightTime;//밤
+                }
             }
             else
             {
-                timeOfDay = 14;//낮
+                timeOfDay += Time.deltaTime;
+                if(timeOfDay < nightTime && timeOfDay > dayTime)
+                {
+                    timeOfDay = dayTime;//낮
+                }
+            }
+        }
+        if (timeOfDay > 23.1)
+        {
+            timeOfDay = 4;
+        }
+        updateLighting();
+    }
+
+    private void updateLighting()
+    {
+        if (directionalLight == null)
+        {
+            Debug.LogError("게임에 디렉셔널 라이트가 존재하지 않습니다.");
+            return;
+        }
+        float timePercent = timeOfDay / 24f;//0 ~ 1
+        directionalLight.transform.localRotation = Quaternion.Euler(new Vector3(timePercent * 360f - 90f, 150f, 0));
+        if(directionalLight.transform.eulerAngles.x >= 180f)//밤
+        {
+            if(directionalLight.color.r > 0.0f)
+            {
+                directionalLight.color -= new Color(1, 1, 1) * Time.deltaTime * 10.0f;
+                if(directionalLight.color.r < 0.0f)
+                {
+                    directionalLight.color = new Color(0, 0, 0);
+
+                    foreach (FunctionLight light in listhLights)
+                    {
+                        light.TurnOnLight(true);
+                    }
+                }
+            }
+        }
+        else if(directionalLight.transform.localRotation.x >= 0.0f)//낮
+        {
+            if(directionalLight.color.r < 1.0f)
+            {
+                directionalLight.color += new Color(1, 1, 1) * Time.deltaTime * 10.0f;
+                if(directionalLight.color.r > 1.0f)
+                {
+                    directionalLight.color = new Color(1, 1, 1);
+
+                    foreach (FunctionLight light in listhLights)
+                    {
+                        light.TurnOnLight(false);
+                    }
+                }
             }
         }
     }
--- Assets/Scripts/FunctionLight.cs
+++ Assets/Scripts/FunctionLight.cs
@@ -4,14 +4,57 @@ using UnityEngine;
 
 public class FunctionLight : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum TypeLight
     {
-        
+        Disable,//ºñ»ç¿ë
+        Allaways,//Ç×»ó
+        OnlyNight,//¹ã¿¡¸¸
+        OnlyDays,//³·¿¡¸¸
     }
 
-    // Update is called once per frame
-    void Update()
+    public TypeLight typeLight;
+
+    Material matWindow;
+    GameObject objLight;
+
+    public void init(bool _isNight)
     {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        matWindow = Instantiate(mr.material);
+        mr.material = matWindow;
+        objLight = GetComponentInChildren<Light>().gameObject;
+
+        //matWindow.EnableKeyword("_EMISSION");//ÄÑÁü
+        
+        if(_isNight == true && typeLight == TypeLight.OnlyNight)
+        {
+            TurnOnLight(true);
+        }
+        else if(_isNight == false && typeLight == TypeLight.OnlyDays)
+        {
+            TurnOnLight(true);
+        }
+        else if(typeLight == TypeLight.Allaways)
+        {
+            TurnOnLight(true);
+        }
+        else
+        {
+            TurnOnLight(false);
+        }
+    }
+
+    public void TurnOnLight(bool _value)
+    {
+        if(_value == true)
+        {
+            matWindow.EnableKeyword("_EMISSON");
+            objLight.SetActive(true);
+        }
+        else
+        {
+            matWindow.DisableKeyword("_EMISSON");//²¨Áü
+            objLight.SetActive(false);
+        }
     }
 }
